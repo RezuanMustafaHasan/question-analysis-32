54f692705fd533bd6ba9f645b8ef3a92ac0eeeb2
Rezuan <hasanideal2002@gmail.com>
2026-05-02 06:40:24 +0600

    added css

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "./index.css";
 import AIQuestionPatternDashboard from "./ai_question_pattern_dashboard.jsx";
 
 createRoot(document.getElementById("root")).render(
